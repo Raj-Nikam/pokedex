8a55eb02690b8c3df17963f4aa00c1b2d63d4ea9
Rajratna Nikam <rnikam@finicity.corp>
2022-05-04 12:57:34 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {Context} from './Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Router>
+    <Context>
+      <App />
+    </Context>
+  </Router>
   </React.StrictMode>
 );
 
